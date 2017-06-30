c03c621930e38d1da076a9a8926fa69054fa0431
Helena Younes Burton <helanayb@cacpwebn-helenay.local>
2017-06-30 13:07:50 -0700

    expe w/ redux

--- src/App.js
+++ src/App.js
@@ -4,8 +4,57 @@ import Card from './Card';
 import MyDictionary from './dictionary.js';
 import { MuiThemeProvider } from 'material-ui/styles';
 import { LabelSwitch } from 'material-ui/Switch';
+import {createStore} from 'redux';
 import './App.css';
 
+var reducer_0 = function (state, action) {
+  console.log('reducer_0 was called with state', state, 'and action', action)
+}
+
+var store_0 = createStore(reducer_0)
+var reducer = function (...args) {
+  console.log('Reducer was called with args', args)
+}
+
+var actionCreator = function() {
+  return {
+    type: 'AN_ACTION'
+  }
+}
+console.log('store_0 state after initialization:', store_0.getState())
+var reducer_1 = function (state, action) {
+  console.log('reducer_1 was called with state', state, 'and action', action)
+  if (typeof state === 'undefined') {
+    return {}
+  }
+  return state;
+}
+
+var store_1 = createStore(reducer_1)
+
+var reducer_2 = function (state = {}, action) {
+  console.log('reducer_2 was called with state', state, 'and action', action)
+  return state;
+}
+var store_2 = createStore(reducer_2)
+console.log('store_2 state after initialization:', store_2.getState())
+
+var reducer_3 = function (state = {}, action) {
+  console.log('reducer_3 was called with state', state, 'and action', action)
+  switch (action.type) {
+    case 'SAY_SOMETHING':
+      return {
+        ...state,
+        message: action.value
+      }
+    default:
+      return state;
+  }
+}
+
+var store_3 = createStore(reducer_3)
+
+console.log('store_3 state after initialization:', store_3.getState())
 
 class App extends Component {
   constructor(props){

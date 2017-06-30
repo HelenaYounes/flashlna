f597949d8b3c93e34c1bbeea1837cd0cda538c89
Helena Younes Burton <helanayb@cacpwebn-helenay.local>
2017-06-30 10:41:51 -0700

    clean up

--- src/App.js
+++ src/App.js
@@ -15,73 +15,51 @@ class App extends Component {
       isFlipped: false,
       currentIndex: 0,
       checkedFrench: false,
-
     }
-
   }
-
-
 
   flipcard() {
     this.setState(({ isFlipped }) => ({ isFlipped: !isFlipped }))
   }
 
   displayEnglishFirst(){
-    if(this.state.isFlipped){
-      return this.state.dictionary[this.state.currentIndex].french
-    }
-
-    return this.state.dictionary[this.state.currentIndex].english
+      return this.state.isFlipped?
+        this.state.dictionary[this.state.currentIndex].french:
+        this.state.dictionary[this.state.currentIndex].english
   }
 
   displayFrenchFirst(){
-    if(this.state.isFlipped){
-      return this.state.dictionary[this.state.currentIndex].english
-    }
-
-    return this.state.dictionary[this.state.currentIndex].french
+    return this.state.isFlipped?
+      this.state.dictionary[this.state.currentIndex].english:
+      this.state.dictionary[this.state.currentIndex].french
   }
 
   displayWord(){
-    if(this.state.checkedFrench){
-      return this.displayFrenchFirst()
-
-    }
-    else{
-     return this.displayEnglishFirst()
-    }
+    return this.state.checkedFrench?
+      this.displayFrenchFirst():this.displayEnglishFirst()
   }
-
 
   getNextCard(){
     this.setState(({ isFlipped }) => ({ isFlipped: false }))
-    if(this.state.currentIndex < this.state.dictionary.length -1){
-      this.setState(({ currentIndex}) => ({ currentIndex: currentIndex +1}))
-    }
-    else {
-        this.setState(({ currentIndex}) => ({ currentIndex: 0}))
-    }
+    this.state.currentIndex < this.state.dictionary.length -1?
+      this.setState(({ currentIndex}) => ({ currentIndex: currentIndex +1})):
+      this.setState(({ currentIndex}) => ({ currentIndex: 0}))
   }
 
   getPrevCard(){
     this.setState(({ isFlipped }) => ({ isFlipped: false }))
-    if(this.state.currentIndex > 0){
-      this.setState(({ currentIndex}) => ({ currentIndex: currentIndex -1}))
+    this.state.currentIndex > 0?
+      this.setState(({ currentIndex}) => ({ currentIndex: currentIndex -1})):
+      this.setState(({ currentIndex}) => ({ currentIndex: this.state.dictionary.length -1}))
     }
-    else {
-        this.setState(({ currentIndex}) => ({ currentIndex: this.state.dictionary.length -1}))
-    }
-  }
 
   switchLangage(){
     this.setState(({ checkedFrench}) => ({checkedFrench: !checkedFrench}))
   }
 
-
   render() {
     return (
       <MuiThemeProvider>
-
         <div className="App">
           <div className="App-header">
             <img src={eiffel} className="Applogo" alt="logo" />
@@ -90,19 +68,15 @@ class App extends Component {
           <LabelSwitch
              onChange={() => this.switchLangage()}
              label="French"
-           />
+          />
           <Card
-          word= {this.displayWord()}
-          onClick={() => this.flipcard()}
-
+            word= {this.displayWord()}
+            onClick={() => this.flipcard()}
           />
           <button className="button" onClick ={() => this.getPrevCard()}>PREVIOUS</button>
-
           <button className="button" onClick ={() => this.getNextCard()}>NEXT</button>
-
         </div>
       </MuiThemeProvider>
-
     );
   }
 }
